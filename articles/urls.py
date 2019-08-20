82373a00a065a31e610c5591a4258b8a5797b214
aaron toliver <flux.charlotte@gmail.com>
2019-08-20 15:40:42 -0400

    id

--- articles/urls.py
+++ articles/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', ArticleListView.as_view(), name='articles-home'),
-    path('<int:pk>', ArticleDetailView.as_view(), name='article-detail')
+    path('<int:id>', ArticleDetailView.as_view(), name='article-detail')
     
 ]
