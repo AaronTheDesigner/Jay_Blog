0e7f37ee5d1172d643029d87a898d886e1b996ce
aaron toliver <flux.charlotte@gmail.com>
2019-08-20 15:19:31 -0400

    hide admin newsletter

--- newsletter/admin.py
+++ newsletter/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import Newsletter
 
 # Register your models here.
-class NewsletterAdmin(admin.ModelAdmin):
-    list_display = ("email", "date_added")
+#class NewsletterAdmin(admin.ModelAdmin):
+ #   list_display = ("email", "date_added")
 
-admin.site.register(Newsletter, NewsletterAdmin)
+#admin.site.register(Newsletter, NewsletterAdmin)
